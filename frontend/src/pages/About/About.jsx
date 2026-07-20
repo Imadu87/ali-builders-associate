ecfefc3c8baa3ebff75759ac2667e7ec71924dab
IMAD UDDIN <120553236+Imadu87@users.noreply.github.com>
2026-07-20 14:32:38 +0500

    Fix import path for ContactCTA component

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -4,7 +4,7 @@ import AboutSection from "../../components/sections/about/AboutSection"
 import StatsSection from "../../components/sections/stats/StatsSection"
 import CEOMessage from "../../components/sections/ceoMessage/CEOMessage"
 import TeamSection from "../../components/sections/team/TeamSection";
-import ContactCTA from "../../components/sections/ContactCTA/ContactCTA";
+import ContactCTA from "../../components/sections/contactCTA/ContactCTA";
 
 import aboutBanner from "../../assets/images/hero/hero1.avif"
 
